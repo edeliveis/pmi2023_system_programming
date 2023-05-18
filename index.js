34edd1e583ee14a6a0c475cd1b9da8552671623b
edeli <edeliveis2002@gmail.com>
2023-05-19 00:39:34 +0300

    updates

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
     $('.header_burger').click(function(event) {
         $('.header_burger, .menu_2').toggleClass('active');
+        $('body').toggleClass('fixed-page');
     });
 });
 
 $('.intro, .redactors').click(function(event) {
     $('.header_burger, .menu_2').removeClass('active');
+    $('body').removeClass('fixed-page');
 });
